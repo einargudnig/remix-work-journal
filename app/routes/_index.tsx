f2484dde55061bf8c7003eda6fe2616d1267cf75
Einar <einargudnig@gmail.com>
2023-11-16 22:03:32 +0100

    built form set up action

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,3 +1,15 @@
+import type { ActionFunctionArgs } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+import { Form } from "@remix-run/react";
+
+export async function action({ request }: ActionFunctionArgs) {
+  const formData = await request.formData()
+  let json = Object.fromEntries(formData)
+  console.log('form data', json)
+
+  return redirect('/')
+}
+
 export default function Index() {
   return (
     <div className="mx-auto max-w-7xl p-6">
@@ -5,6 +17,43 @@ export default function Index() {
       <p className="mt-3 text-xl text-gray-400">
         Doings and learnings. Updated weekly.
       </p>
+
+      <div className="my-8 border p-3">
+        <Form method="post">
+          <p className="italic">Create an entry</p>
+
+          <div className="mt-4">
+            <div>
+              <input type="date" name="date" className="text-gray-700" />
+            </div>
+
+            <div className="mt-2 space-x-6">
+              <label>
+                <input className="mr-1" type="radio" name="category" value="work" />
+                Work
+              </label>
+              <label>
+                <input className="mr-1" type="radio" name="category" value="learning" />
+                Learnings
+              </label>
+              <label>
+                <input className="mr-1" type="radio" name="category" value="interesting-thing" />
+                Interesting thing
+              </label>
+            </div>
+
+            <div className="mt-2">
+              <textarea name="text" className="w-full text-gray-700" placeholder="Write your entry..." />
+            </div>
+
+            <div className="mt-1 text-right">
+              <button type="submit" className="bg-blue-500 px-4 py-1 font-medium text-white">
+                Save
+              </button>            
+            </div>
+          </div>
+        </Form>
+      </div>
 
       <div className="mt-8">
         <ul>
